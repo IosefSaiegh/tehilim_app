b1c7739ef6d8b491effe38218f0a30e9a74952ae
IosefSaieg <saiegh.saiegh@gmail.com>
2022-02-02 17:13:48 -0300

    nada importante

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:splash_screen_view/SplashScreenView.dart';
 
 import 'package:tehilim/screens/pantalla_p.dart';
 
 void main() {
   runApp(MyApp());
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.red,
-  ));
 }
 
 class MyApp extends StatefulWidget {
